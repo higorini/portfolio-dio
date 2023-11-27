c4d3b1eab34d031ef16c276443dcedc056d184b4
higorini <108697425+higorini@users.noreply.github.com>
2023-11-27 01:22:51 -0300

    Little modification

--- assets/js/typing.js
+++ assets/js/typing.js
@@ -1,11 +1,13 @@
 var title = document.getElementById("title");
 
 var Typewriter = new Typewriter(title, {
-  loop: true,
+  loop: false,
   delay: 75,
 });
 
 Typewriter.pauseFor(2000)
-  .typeString("Olá,<br />eu sou o<br />Higor M. dos Santos")
-  .start()
-  .pauseFor(28000);
+  .typeString("Olá,<br />eu sou o<br />Higor Moraes")
+  .pauseFor(300)
+  .deleteChars(5)
+  .typeString(". dos Santos")
+  .start();
